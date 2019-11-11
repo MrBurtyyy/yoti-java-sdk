5fcb0122826060e88f23d0159052a56f194e1d86
Alex Burt <alex.burt@yoti.com>
2019-11-11 16:52:35 +0000

    SDK-1264: Implement EncryptedDataReader for reading extra data out of the receipt

---
+++ yoti-sdk-impl/src/main/java/com/yoti/api/client/spi/remote/ExtraDataReader.java
@@ -0,0 +1,33 @@
+package com.yoti.api.client.spi.remote;
+
+import com.yoti.api.client.ExtraData;
+import com.yoti.api.client.ExtraDataException;
+import com.yoti.api.client.ProfileException;
+
+import java.security.Key;
+
+public class ExtraDataReader extends EncryptedDataReader {
+
+    private final ExtraDataConverter extraDataConverter;
+
+    private ExtraDataReader(ExtraDataConverter extraDataConverter) {
+        this.extraDataConverter = extraDataConverter;
+    }
+
+    static ExtraDataReader newInstance() {
+        return new ExtraDataReader(
+                ExtraDataConverter.newInstance()
+        );
+    }
+
+    @Override
+    ExtraData read(byte[] encryptedBytes, Key secretKey) throws ProfileException, ExtraDataException {
+        ExtraData extraData = null;
+        if (encryptedBytes != null || encryptedBytes.length > 0) {
+            byte[] extraDataBytes = decryptBytes(encryptedBytes, secretKey);
+            extraData = extraDataConverter.read(extraDataBytes);
+        }
+
+        return extraData;
+    }
+}
